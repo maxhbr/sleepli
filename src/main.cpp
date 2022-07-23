8701bc7f0274f74f4b2cf02f0418148466a5e750
Maximilian Huber <gh@maxhbr.de>
2022-07-23 22:08:36 +0200

    digispark-tiny -> adafruit_trinket_m0
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,78 @@
 #include "Arduino.h"
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
+#include <Adafruit_DotStar.h>
 
-#ifndef LED_BUILTIN
 #define LED_BUILTIN 13
-#endif
 
-#define PIN PCINT4
-#define DELAYVAL 500
-#define NUMPIXELS 7
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define NUMPIXELS  1 
+#define DATAPIN    7
+#define CLOCKPIN   8
+Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
+#define RADAR 2
+
+#define RUN_LENGTH 480000
+#define INITIAL_PULSE_WIDTH 8000
+#define PULSE_INCREMENT 100
+#define MAX_BRIGHTNESS 100
+
+int running = 0;
+unsigned long startTime;
 unsigned long pulseStartTime;
-unsigned long pulseDuration = 4000;
+unsigned long pulseDuration = INITIAL_PULSE_WIDTH;
 
-void setup()
-{
-
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-      clock_prescale_set(clock_div_1);
-#endif
+void setup() {
+    Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
-    pixels.begin();
+    pinMode (RADAR, INPUT);
+    strip.begin(); // Initialize pins for output
+    for(int i=0; i<strip.numPixels(); i++) { 
+        strip.setPixelColor(i, 0,255,0);
+    }
+    strip.setBrightness(0);
+    strip.show();  // Turn all LEDs off ASAP
+    delay(3000);
 }
 
 void setBrightness(int b) {
-    for(int i=0; i<NUMPIXELS; i++) {
-        pixels.setPixelColor(i, pixels.Color(b, 0, 0));
-    }
-    pixels.show();
+    strip.setBrightness(b);
+    strip.show();
 }
 
 void startPulse() {
     pulseStartTime = millis();
 }
 
-void loop()
-{
-    unsigned long curTime = millis();
-    unsigned long timePassed = curTime - pulseStartTime;
-    if (timePassed > pulseDuration) {
-        pulseDuration = pulseDuration + 100;
-        startPulse();
-    } else {
-        setBrightness((1-cos(2 * 3.15 * timePassed / pulseDuration))*30);
-    }
+void start() {
+    startTime = millis();
+    pulseDuration = INITIAL_PULSE_WIDTH;
+    startPulse();
+}
 
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(10);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(10);
+void loop() {
+    if (running) {
+        unsigned long curTime = millis();
+        unsigned long timePassed = curTime - pulseStartTime;
+        if (timePassed > pulseDuration) {
+            pulseDuration = pulseDuration + PULSE_INCREMENT;
+            if (curTime - startTime > RUN_LENGTH) {
+                running = 0;
+                return;
+            }
+            startPulse();
+        } else {
+            setBrightness((1-cos(2 * 3.15 * timePassed / pulseDuration))*MAX_BRIGHTNESS);
+        }
+    } else {
+        strip.setBrightness(0);
+        delay(100);
+
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(10);
+        digitalWrite(LED_BUILTIN, LOW);
+
+        running = digitalRead(RADAR);
+        if (running) {
+            start();
+        }
+    }
 }
